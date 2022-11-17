32d97251e1249483125724ef74e1bf5ab227b931
Hans Raj <hans63565@gmail.com>
2022-11-17 19:32:50 +0500

    Written the application logic in the PaintView class and MainActivity

--- app/src/main/java/com/example/paintapplication/MainActivity.kt
+++ app/src/main/java/com/example/paintapplication/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.example.paintapplication
 
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import com.example.paintapplication.PaintView.Companion.colorList
+import com.example.paintapplication.PaintView.Companion.currentBrush
+import com.example.paintapplication.PaintView.Companion.pathList
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        var path = Path()
+        var paintBrush = Paint()
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
 
         val redColor = findViewById<ImageButton>(R.id.redColor)
         val yellowColor = findViewById<ImageButton>(R.id.yellowColor)
@@ -15,17 +30,29 @@ class MainActivity : AppCompatActivity() {
         val eraser = findViewById<ImageButton>(R.id.whiteColor)
 
         redColor.setOnClickListener{
+            paintBrush.color = Color.RED
+            currentColor(paintBrush.color)
 
         }
         yellowColor.setOnClickListener{
+            paintBrush.color = Color.YELLOW
+            currentColor(paintBrush.color)
 
         }
         blackColor.setOnClickListener{
+            paintBrush.color = Color.BLACK
+            currentColor(paintBrush.color)
 
         }
         eraser.setOnClickListener{
-
+            pathList.clear()
+            colorList.clear()
+            path.reset()
         }
 
+    }
+    private fun currentColor(color:Int){
+        currentBrush = color
+        path = Path()
     }
 }
